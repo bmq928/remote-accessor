6e17730cec7f1fa36f38a8db0e92970b2afc4e5d
bmq928 <bmq928@gmail.com>
2019-11-02 11:07:05 +0700

    frontend

--- agents/vendors.js
+++
@@ -1,6 +0,0 @@
-module.exports.sensors = {}
-module.exports.sensors.processMonitor = require('../sensors/process-monitor')
-module.exports.sensors.fileExplorer = require('../sensors/file-explorer')
-
-// module.exports.logging = require('../shares/logging')
-module.exports.logging = require('../shares/logging')
--- sensors/file-explorer/src/index.js
+++
@@ -1,3 +0,0 @@
-module.exports.readFileContent = require('./read-file-content')
-module.exports.listFolderItems = require('./list-folder-items')
-module.exports.showFolderTree = require('./show-folder-tree')
