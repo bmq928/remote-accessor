4789049f48d3d0c1550d8c08c7cb159a41c14ecc
bmq928 <bmq928@gmail.com>
2019-11-24 16:02:13 +0700

    fix ui

--- frontend/src/pages/FileStoragePage/FileRepresentation/FileRepresentation.js
+++ frontend/src/pages/FileStoragePage/FileRepresentation/FileRepresentation.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useGlobal } from 'reactn'
+import shortid from 'shortid'
 
 import { FileItem } from '../vendors'
 import './FileRepresentation.scss'
@@ -11,7 +12,7 @@ export default function FileRepresentation() {
   const NUM_ITEM_PER_ROW = 6
   const fileOrFolders = rootNode.children
   const rows = splitArrayIntoChunkWithLength(fileOrFolders, NUM_ITEM_PER_ROW)
-  
+
   function splitArrayIntoChunkWithLength(arr, chunkLength) {
     const chunks = []
     const numChunk = Math.floor(
@@ -24,7 +25,11 @@ export default function FileRepresentation() {
       const chunk = arr.filter(
         (val, idx) => Math.floor(idx / chunkLength) === i
       )
-      if (chunk.length) chunks.push(chunk)
+
+      if (chunk.length) {
+        chunk.id = shortid.generate()
+        chunks.push(chunk)
+      }
     }
     return chunks
   }
@@ -38,26 +43,34 @@ export default function FileRepresentation() {
     setItemSelected([])
   }
 
-  function openItem() {
-
-  }
+  function openItem() {}
 
   return (
-    <div className="FileRepresentation" onClick={() => deSelectAllItem()}>
-      {rows.map((row, idx) => (
-        <div className="columns" key={idx}>
+    <div
+      className="FileRepresentation"
+      onClick={() => deSelectAllItem()}
+      onKeyDown={() => {}}
+    >
+      {rows.map(row => (
+        <div className="columns" key={row.id}>
           {row.map(item => (
             <div
               key={item.path}
               className={
                 itemSelected.includes(item) ? 'column item-selected' : 'column'
               }
-              onClick={(evt) => selectItem(evt,item)}
-              onDoubleClick={(evt) => openItem(evt, item)}
+              onClick={evt => selectItem(evt, item)}
+              onDoubleClick={evt => openItem(evt, item)}
+              onKeyUp={() => {}}
             >
               <FileItem itemPath={item.path} isFolder={!item.isFile} />
             </div>
           ))}
+          {// the rest part to make sure each row have enough
+          new Array(NUM_ITEM_PER_ROW - row.length).fill().map(() => (
+            <div key={shortid.generate()} className="column" />
+          ))
+          }
         </div>
       ))}
     </div>
