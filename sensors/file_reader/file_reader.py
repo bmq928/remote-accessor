35da1146cd932c68926005c203de729d45f6fe29
Bui Minh Quang <bmq928@gmail.com>
2019-11-30 21:52:16 +0700

    done file-preview

--- sensors/file_reader/file_reader.py
+++ sensors/file_reader/file_reader.py
@@ -8,40 +8,69 @@ class FileNotExisted(Exception):
 
 
 def read_file(file_path: str) -> dict:
-    is_txt = check_is_txt_file(file_path)
-    mime = 'text' if is_txt else 'binary'
-    if not path.exists(file_path):
-        return {
-            'mime': mime,
-            'content': None,
-            'error': 'File is not existed'
-        }
+    is_txt, ext = check_is_txt_file(file_path)
+    is_file_existed = path.exists(file_path)
+    error_msg = "File is not existed" if not is_file_existed else None
+    file_content = (
+        None
+        if error_msg
+        else (read_txt_content(file_path) if is_txt else read_binary_content(file_path))
+    )
 
-    content = read_txt_content(file_path) if is_txt \
-        else read_binary_content(file_path)
     return {
-        'mime': mime,
-        'content': content,
-        'error': None
+        "is_text": is_txt,
+        "content": file_content,
+        "error": error_msg,
+        "ext": ext,
     }
+
+    # if not path.exists(file_path):
+    #     return {
+    #         "is_text": is_txt,
+    #         "content": None,
+    #         "error": "File is not existed",
+    #         "ext": ext,
+    #     }
+
+    # content = read_txt_content(file_path) if is_txt else read_binary_content(file_path)
+    # return {"is_text": is_txt, "content": content, "error": None, "ext": ext}
 
 
 def check_is_txt_file(file_path: str) -> bool:
+    EXTRA_TEXT_EXTS = [
+        ".js",
+        "",
+        ".scss",
+        ".css",
+        ".txt",
+        ".py",
+        ".html",
+        ".yaml",
+        ".yml",
+        ".json",
+        ".md",
+        ".gitignore",
+    ]
+
+    _, file_ext = path.splitext(file_path)
     file_type, _ = mimetypes.guess_type(file_path)
+
+    if file_ext in EXTRA_TEXT_EXTS:
+        return True, file_ext
     if file_type == None:
-        return False
-    is_txt = file_type.split('/')[0] == 'text'
-    return is_txt
+        return False, file_ext
+    is_txt = file_type.split("/")[0] == "text"
+    return is_txt, file_ext
 
 
 def read_txt_content(file_path: str) -> str:
-    with open(file_path, 'r') as f:
+    with open(file_path, "r") as f:
         return f.read()
 
 
 def read_binary_content(file_path: str):
-    with open(file_path, 'rb') as f:
+    with open(file_path, "rb") as f:
         raw_content = f.read()
         base64_encoded = base64.b64encode(raw_content)
-        base64_decoded = base64_encoded.decode('utf8')
+        base64_decoded = base64_encoded.decode("utf8")
         return base64_decoded
